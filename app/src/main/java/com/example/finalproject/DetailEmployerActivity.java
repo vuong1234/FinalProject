ca78d1de028da056a453c697e87a20f1e98592b1
Quang Anh <bantia447@gmail.com>
2020-12-31 15:11:38 +0700

    Final-Project

---
+++ app/src/main/java/com/example/finalproject/DetailEmployerActivity.java
@@ -0,0 +1,30 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetailEmployerActivity extends AppCompatActivity {
+    private ImageView imgLogoEmployer;
+    private TextView txtNameEmployerDetail,txtAddressEmployerDetail,txtWelfareEmployerDetail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_employer);
+
+        imgLogoEmployer = findViewById(R.id.imgLogoEmployer);
+        txtNameEmployerDetail = findViewById(R.id.txtNameEmployerDetail);
+        txtAddressEmployerDetail = findViewById(R.id.txtAddressEmployerDetail);
+        txtWelfareEmployerDetail = findViewById(R.id.txtWelfareEmployerDetail);
+
+        Bundle bundle = getIntent().getExtras();
+        txtNameEmployerDetail.setText(bundle.getString("nameEmpoyer").toString());
+        txtAddressEmployerDetail.setText(bundle.getString("addressEmpoyer"));
+        txtWelfareEmployerDetail.setText(bundle.getString("welfareEmployer"));
+        Picasso.get().load(bundle.getString("logoEmployer")).into(imgLogoEmployer);
+    }
+}
---
+++ app/src/main/java/com/example/finalproject/IntroActivity.java
@@ -0,0 +1,201 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter ;
+    TabLayout tabIndicator;
+    Button btnNext;
+    int position = 0 ;
+    Button btnGetStarted;
+    Animation btnAnim ;
+    TextView tvSkip;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // make the activity on full screen
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        // kiem tra xem sharepreference de nhan biet lan dau vào app
+
+        if (restorePrefData()) {
+
+            Intent mainActivity = new Intent(getApplicationContext(),MainActivity.class );
+            startActivity(mainActivity);
+            finish();
+
+
+        }
+
+        setContentView(R.layout.activity_intro);
+
+        //mapping
+        btnNext = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_get_started);
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_getstarted_anim);
+        tvSkip = findViewById(R.id.tv_skip);
+
+       //add list screen intro
+
+        final List<ScreenItem> mList = new ArrayList<>();
+        mList.add(new ScreenItem("Tìm kiếm","Nếu bạn đang lo vấn đề kiếm việc giữa mùa Covid vì không thể ra ngoài, FindJob sẽ giúp bạn",R.drawable.image1));
+        mList.add(new ScreenItem("Linh động và nhanh chóng","Thuận lợi, nhanh chóng, lựa chọn công việc phù hợp ngày trước màn hình, xem thông tin và những phúc lợi từ nhà tuyển dụng",R.drawable.image2_gif));
+        mList.add(new ScreenItem("Trao đổi thông tin","Gửi CV và đợi nhà tuyển dụng trực tiếp liên lạc mà không cần đi lại giữa mùa Covid",R.drawable.image3_gif));
+
+        // setup viewpager
+        screenPager =findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this,mList);
+        screenPager.setAdapter(introViewPagerAdapter);
+
+        // setup tablayout with viewpager
+
+        tabIndicator.setupWithViewPager(screenPager);
+
+        // next button click Listner
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                position = screenPager.getCurrentItem();
+                if (position < mList.size()) {
+                    position++;
+                    screenPager.setCurrentItem(position);
+
+                }
+
+                if (position == mList.size()-1) { // when we rech to the last screen
+
+                    loaddLastScreen();
+
+                }
+
+
+
+            }
+        });
+
+        // tablayout add change listener
+
+        tabIndicator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == mList.size()-1) {
+
+                    loaddLastScreen();
+
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+        // Get Started button click chuyen intent login || register
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //open main activity
+
+                Intent mainActivity = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(mainActivity);
+                // also we need to save a boolean value to storage so next time when the user run the app
+                // we could know that he is already checked the intro screen activity
+                // i'm going to use shared preferences to that process
+                savePrefsData();
+                finish();
+
+
+
+            }
+        });
+
+        // skip button click listener
+
+        tvSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screenPager.setCurrentItem(mList.size());
+            }
+        });
+
+
+
+    }
+
+    private boolean restorePrefData() {
+
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        Boolean isIntroActivityOpnendBefore = pref.getBoolean("isIntroOpnend",false);
+        return  isIntroActivityOpnendBefore;
+
+
+
+    }
+
+    private void savePrefsData() {
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isIntroOpnend",true);
+        editor.commit();
+
+
+    }
+
+    // show the GETSTARTED Button and hide the indicator and the next button
+    private void loaddLastScreen() {
+
+        btnNext.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tvSkip.setVisibility(View.INVISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+        btnGetStarted.setAnimation(btnAnim);
+
+
+
+    }
+}
